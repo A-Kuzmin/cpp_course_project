94859c0af85b846468b89afbe534e01391b5e5a0
Alexandr Kuzmin <kuzmin-1994@yandex.ru>
2017-04-01 13:15:12 +0400

    array 3 array 4

--- cpp_cource_project/cpp_cource_project.cpp
+++ cpp_cource_project/cpp_cource_project.cpp
@@ -1,39 +1,52 @@
-// cpp_cource_project.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
 
 using namespace std;
-void printArray(int* arr, int sizeArr);
-bool checkValue(int val);
-void readArray(string filename, int* arr, int arrSize);
-void prepareArray1();
-void prepareArray2();
-int getMaxItemIndex(int* arr, int sizeArr);
-int getMinItemIndex(int* arr, int sizeArr);
-int getMaxMinItemIndex(int* arr, int sizeArr, bool needMax);
-void sortArray(int* arr, int sizeArr, bool asc);
 
 const int
 array1Size = 10,
 array2Size = 10,
 array3Size = 10,
-array4Size = 10;
+array4Size = 10,
 
-const int
 rangeMinValue = 0,
 rangetMaxValue = 5;
-
 const bool includeBoundary = true;
 
 int main()
 {
-	setlocale(LC_ALL, "RUSSIAN");
-	prepareArray1();
-	prepareArray2();
-	system("pause");
+	init();
+	bool needExit = false;
+	int command;
+	do {
+		showMenu();
+		cin >> command;
+		
+		switch (command) {
+			case 0: 
+				needExit = true;
+				break;
+			case 1:
+				prepareArray1();
+				break;
+			case 2:
+				prepareArray2();
+				break;
+			case 3:
+				prepareArray3();
+				break;
+			case 4:
+				prepareArray4();
+				break;
+		    default:
+			    break;
+		}
+		if (!needExit) {
+			needExit = endComand();
+		}
+	} while (!needExit);
     return 0;
 }
+
 void prepareArray1()
 {
 	int* arr = new int[array1Size];
@@ -76,26 +89,118 @@ void prepareArray2()
 	cout << "Массив содержит " << count << " нулей.\n";
 }
 
-int getMaxItemIndex(int* arr, int sizeArr)
-{
-	int index = 0;
 
-	for (int i = 1; i < sizeArr; i++)
-	{
-		if (arr[i] > arr[index])  index = i;
+
+void prepareArray3()
+{
+	int* arr = new int[array3Size * 2];
+	int size = array3Size;
+
+	for (int i = 0; i < size; i++)
+		arr[i] = generateValue();
+
+	cout << "Массив 3 исходный массив:\n";
+	printArray(arr, size);
+
+	int maxValue = arr[getMaxMinItemIndex(arr, size, true)];
+
+	for (int i = 0; i < size; i++) {
+		if (arr[i] == 0) {
+			i++;
+			insert(arr, i, maxValue, size);
+		}
 	}
-	return index;
+	cout << "Массив 3 результат:\n";
+	printArray(arr, size);
+
+	cout << "Массив 3 сортированный результат:\n";
+	sortArray(arr, size, true);
+	printArray(arr, size);
 }
 
-int getMinItemIndex(int* arr, int sizeArr)
+void prepareArray4()
 {
-	int index = 0;
+	int* arr = new int[array4Size * 2];
+	int size = array4Size;
+	int el;
 
-	for (int i = 1; i < sizeArr; i++)
-	{
-		if (arr[i] < arr[index])  index = i;
+
+	cout << "Введите массив:\n";
+	for (int i = 0; i < size; i++) {
+		cin >> el;
+		if (checkValue(el)) {
+			arr[i] = el;
+		}
+		else {
+			cout << "Не корректное число " << el << "\n";
+			i--;
+		}
 	}
-	return index;
+
+	cout << "Массив 4 исходный массив:\n";
+	printArray(arr, size);
+
+	int from = getMaxMinItemIndex(arr, array1Size, true);
+	int to = getMaxMinItemIndex(arr, array1Size, false);
+
+	if (from > to) {
+		int temp = to;
+		to = from;
+		from = temp;
+	}
+	
+	for (int index = to - 1; index > from; index--) {
+		remove(arr, index, size);
+	}
+
+	cout << "Массив 4 результат:\n";
+	printArray(arr, size);
+}
+
+void init()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    srand(unsigned(time(0)));
+}
+
+void showMenu()
+{
+    cout << "Введите команду:\n";
+    cout << "1: Массив 1.\n";
+    cout << "2: Массив 2.\n";
+    cout << "3: Массив 3.\n";
+    cout << "4: Массив 4.\n";
+
+    cout << "0: Выйти\n";
+}
+bool endComand()
+{
+    int command;
+    cout << "Введите команду:\n";
+    cout << "0 для выхода\n";
+    cout << "любую другую цифру для вывода меню\n";
+    cin >> command;
+    return command == 0;
+}
+
+
+void insert(int* arr, int index, int elem, int &sizeArr)
+{
+	for (int j = sizeArr - 1; j >= index; j--)
+	{
+		arr[j + 1] = arr[j];
+	}
+	arr[index] = elem;
+	sizeArr++;
+}
+
+void remove(int* arr, int index, int &sizeArr)
+{
+	sizeArr--;
+	for (int i = index; i < sizeArr; i++)
+	{
+		arr[i] = arr[i + 1];
+	}
 }
 
 int getMaxMinItemIndex(int* arr, int sizeArr, bool needMax)
@@ -158,34 +263,30 @@ void readArray(string filename, int* arr, int arrSize)
 	int temp;
 
 	ifstream in(filename);
-	if (in.is_open())
-	{
+	if (in.is_open()) {
 		try
 		{
 			int i = 0;
-			while (!in.eof() && (i < arrSize))
-			{
+			while (!in.eof() && (i < arrSize)) {
 				in >> temp;
 
 				if (checkValue(temp)) {
 					arr[i] = temp;
 					i++;
-				}
-				else {
+				} else {
 					throw new exception("Не корректное число в файле.");
 				}
 
 			}
-		}
-		catch (const std::exception& ex)
-		{
+            for (; i < arrSize; i++) {
+                arr[i] = 0;
+            }
+		} catch (const std::exception& ex) {
 			in.close();
 			throw ex;
 		}
 		in.close();
-	}
-	else
-	{
+	} else {
 		throw new exception("Файл не найден");
 	}
 }
--- cpp_cource_project/stdafx.h
+++ cpp_cource_project/stdafx.h
@@ -1,8 +1,3 @@
-// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
-// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
-// не часто измен€ютс€
-//
-
 #pragma once
 
 #include "targetver.h"
@@ -12,7 +7,21 @@
 #include <iostream>
 #include <fstream>
 #include <tchar.h>
+#include "time.h"
 
 
-
-// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
+void printArray(int* arr, int sizeArr);
+bool checkValue(int val);
+void prepareArray1();
+void prepareArray2();
+void prepareArray3();
+void prepareArray4();
+void init();
+void insert(int* arr, int index, int elem, int &sizeArr);
+void remove(int* arr, int index, int &sizeArr);
+int generateValue();
+int getMaxMinItemIndex(int* arr, int sizeArr, bool needMax);
+void sortArray(int* arr, int sizeArr, bool asc);
+void showMenu();
+bool endComand();
+void readArray(std::string filename, int* arr, int arrSize);
